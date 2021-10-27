4cecfe8bb1ea8bfe2f68ffb7817769c495047627
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2021-10-27 21:14:13 +0300

    Added Article & News components

---
+++ News4Devs/News4Devs.Client/Components/Article.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using News4Devs.Core.DTOs;
+
+namespace News4Devs.Client.Components
+{
+    public partial class Article
+    {
+        [Parameter]
+        public ArticleDto ArticleDto { get; set; }  
+    }
+}
---
+++ News4Devs/News4Devs.Client/Components/News.razor.cs
@@ -0,0 +1,73 @@
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using News4Devs.Client.Models;
+using News4Devs.Client.Services.Interfaces;
+using News4Devs.Core.DTOs;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace News4Devs.Client.Components
+{
+    public partial class News
+    {
+        [Inject]
+        private IHttpClientService HttpClientService { get; set; }
+
+        [Inject]
+        private IToastService ToastService { get; set; }
+
+        private List<ArticleDto> articles = new List<ArticleDto>();
+        private int numberOfTotalPages;
+        private int pageNumber = 1;
+
+        protected async override Task OnInitializedAsync()
+        {
+            string url = GetUrl();
+            var response = await HttpClientService.GetAsync<NewsCatcherApiResponseDto>(url);
+
+            HandleApiResponse(response);
+        }
+
+        private async Task LoadMoreArticles()
+        {
+            string url = GetUrl();
+            var response = await HttpClientService.GetAsync<NewsCatcherApiResponseDto>(url);
+
+            HandleApiResponse(response);
+        }
+
+        private int GetNextPageNumber()
+        {
+            if (pageNumber + 1 < numberOfTotalPages) 
+            {
+                return pageNumber + 1;
+            }
+            return pageNumber;
+        }
+
+        private string GetUrl()
+        {
+            return $"{ClientConstants.BaseUrl}v1/news/?q=dotnet&lang=en&page={pageNumber}";
+        }
+
+        private void HandleApiResponse(ApiResponse<NewsCatcherApiResponseDto> response)
+        {
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                ToastService.ShowError("An unexpected error occured during retrieving the news");
+            }
+            else
+            {
+                var newsCatcherApiResponse = response.Data;
+                numberOfTotalPages = newsCatcherApiResponse.total_pages;
+                if (newsCatcherApiResponse.articles != null)
+                {
+                    articles.AddRange(newsCatcherApiResponse.articles);
+                }
+                // compute the next page number
+                pageNumber = GetNextPageNumber();
+            }
+        }
+    }
+}
